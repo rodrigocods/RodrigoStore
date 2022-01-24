07a84bfe59b45a22773ad21aed81d12c224216d4
rodrigocods <rodrigomengo06@gmail.com>
2022-01-24 09:04:25 -0300

    delete old files

--- database_model/Insert.sql
+++
@@ -1,9 +0,0 @@
-USE rodrigostore;
-
-INSERT INTO product VALUES(5, "Computador Apple ALL IN ONE", "Um ALL IN ONE de ultima geração da Apple", 3999.99, 200);
-INSERT INTO product VALUES(6, "Fogão quatro bocas", "Um Fogão quatro bocas com forno integrado", 899.99, 200);
-INSERT INTO product VALUES(7, "Smartphone Xiaomi m1 8gb 64gb", "Smartphone de última geração da Xiaomi com 8gb de ram e 64gb de armazenamento para poder colocar todas as suas e fotos e seus apps", 3999.99, 200);
-INSERT INTO product VALUES(8, "Geladeira Consul", "Geladeira Consul modelo 2021 com refrigerador imbutido", 799.99, 200);
-INSERT INTO product VALUES(9, "Secador de cabelo", "Um secador de cabelo muito bom", 499.99, 200);
-INSERT INTO product VALUES(10, "Kit Camera Canon com 8 lentes e Case", "Um kit completo para fotografia profissional com camera, 8 lentes e case para transporte dos equipamentos", 2999.99, 200);
-INSERT INTO product VALUES(11, "Headphone bluetooth JBL", "Fone de última geração com som espacial e cancelamento de ruído", 999.99, 200);
