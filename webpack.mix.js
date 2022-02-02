4e33e954f7112ce08398c7de5359f67c572e79b1
rodrigocods <rodrigomengo06@gmail.com>
2022-02-02 17:33:15 -0300

    add jquery and bootstrap by npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/plugins/bootstrap/bootstrap.css')
+    .scripts('node_modules/jquery/dist/jquery.min.js', 'public/plugins/jquery/jquery.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/plugins/bootstrap/bootstrap.js');
+
